0f9fee0b8bf30932a77468ca195e27305061ce2c
Lukasz GitExerc <lukaszgitexerc@zajavka.pl>
2023-05-15 10:14:48 +0200

    Person class modification 1

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -2,13 +2,23 @@ package pl.zajavka.case2;
 
 import java.math.BigDecimal;
 
-record Person(String name, BigDecimal salary) {
+record Person(String name, String surname, BigDecimal salary) {
 
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
+    }
     public BigDecimal howMuchDoYouEarn() {
         return salary;
     }
-
     public Boolean doYouLikePizza() {
+        return true;
+    }
+
+    public Boolean doYouHaveABike() {
         return true;
     }
 }
